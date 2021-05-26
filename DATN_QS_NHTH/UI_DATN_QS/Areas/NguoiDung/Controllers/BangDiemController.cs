129487188076e39f1a4c73bf6af43f535622dd6c
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2021-05-27 00:34:39 +0700

    .

---
+++ DATN_QS_NHTH/UI_DATN_QS/Areas/NguoiDung/Controllers/BangDiemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI_DATN_QS.Areas.NguoiDung.Controllers
+{
+    public class BangDiemController : Controller
+    {
+        // GET: NguoiDung/BangDiem
+        public ActionResult GET_BangDiemLop()
+        {
+            return View();
+        }
+    }
+}
